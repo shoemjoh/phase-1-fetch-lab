3bd6af510355306688aa4c0c2266a8fca2c74fdd
John <shoemjoh@gmail.com>
2023-11-19 18:23:59 -0500

    fetch lab completed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => {
+      renderBooks(json)
+    });
+
+
 }
 
 function renderBooks(books) {
@@ -12,6 +18,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
